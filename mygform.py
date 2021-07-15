45184009b090b75a32361ce6d8f7407a35d9ad4c
Mithunprb <61266869+Mithunprb@users.noreply.github.com>
2021-07-16 00:49:46 +0530

    updated script

--- mygform.py
+++ mygform.py
@@ -42,7 +42,7 @@ for data in datas:
     check1_2 = driver.find_element_by_xpath(
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div[1]/div/span/div/div[3]/label')
 
-    check1 = func = random.choice([check1_0, check1_1, check1_2])
+    check1  = random.choice([check1_0, check1_1, check1_2])
     check1.click()
 
     check2_0 = driver.find_element_by_xpath(
@@ -57,7 +57,7 @@ for data in datas:
     check2_3 = driver.find_element_by_xpath(
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div[1]/div/span/div/div[4]/label')
     
-    check2 = func = random.choice([check2_0, check2_1, check2_2, check2_3])
+    check2  = random.choice([check2_0, check2_1, check2_2, check2_3])
     check2.click()
 
     check3_0 = driver.find_element_by_xpath(
@@ -75,28 +75,28 @@ for data in datas:
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[6]/div/div/div[2]/div[1]/div/span/div/div[6]/label')
     check4_2 = driver.find_element_by_xpath(
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[6]/div/div/div[2]/div[1]/div/span/div/div[5]/label')
-    check4 = func = random.choice([check4_0, check4_1, check4_2])
+    check4  = random.choice([check4_0, check4_1, check4_2])
     check4.click()
 
     check5_0 = driver.find_element_by_xpath(
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[7]/div/div/div[2]/div[1]/div/span/div/div[1]/label')
     check5_1 = driver.find_element_by_xpath(
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[8]/div/div/div[2]/div[1]/div/span/div/div[2]/label')
-    check5 = func = random.choice([check5_0, check5_1])
+    check5  = random.choice([check5_0, check5_1])
     check5.click()  
 
     check6_0 = driver.find_element_by_xpath(
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[8]/div/div/div[2]/div[1]/div/span/div/div[1]/label')
     check6_1 = driver.find_element_by_xpath(
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[8]/div/div/div[2]/div[1]/div/span/div/div[2]/label')
-    check6 = func = random.choice([check6_0, check6_1])
+    check6  = random.choice([check6_0, check6_1])
     check6.click()
 
     check7_0 = driver.find_element_by_xpath(
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[9]/div/div/div[2]/div[1]/div/span/div/div[1]/label')
     check7_1 = driver.find_element_by_xpath(
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[9]/div/div/div[2]/div[1]/div/span/div/div[2]/label')
-    check7 = func = random.choice([check7_0, check7_1])
+    check7  = random.choice([check7_0, check7_1])
     check7.click()
 
     check8_0 = driver.find_element_by_xpath(
@@ -107,14 +107,14 @@ for data in datas:
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[10]/div/div/div[2]/div[1]/div/span/div/div[2]/label')
     check8_3 = driver.find_element_by_xpath(
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[10]/div/div/div[2]/div[1]/div/span/div/div[4]/label')
-    check8 = func = random.choice([check8_0, check8_1, check8_2, check8_3])
+    check8  = random.choice([check8_0, check8_1, check8_2, check8_3])
     check8.click()
 
     check9_0 = driver.find_element_by_xpath(
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[11]/div/div/div[2]/div[1]/div/span/div/div[1]/label')
     check9_1 = driver.find_element_by_xpath(
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[11]/div/div/div[2]/div[1]/div/span/div/div[2]/label')
-    check9 = func = random.choice([check9_0, check9_1])
+    check9  = random.choice([check9_0, check9_1])
     check9.click()
                                                    # Iterate through all input boxes
     for value in textboxes:
